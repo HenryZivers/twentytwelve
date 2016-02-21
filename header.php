81e706c2c5b3609de179c7254ec68099982cd774
Henry Zhang <zivers@live.cn>
2016-02-21 18:09:40 +0800

    update

--- header.php
+++ header.php
@@ -47,7 +47,7 @@ var _hmt = _hmt || [];
 	<header id="masthead" class="site-header" role="banner">
 		<hgroup>
 			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" style="font-family: Microsoft YaHei;font-weight: normal" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<h2 class="site-description">&nbsp-&nbsp<?php bloginfo( 'description' ); ?></h2>
+			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 		</hgroup>
 			
 		<nav id="site-navigation" class="main-navigation" role="navigation">
